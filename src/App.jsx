8c4a077a388510a32f739715760c460960b89477
ShowyCone <alexrafael2003@gmail.com>
2025-05-27 01:56:18 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import MedicalCalendar from './MedicalCalendar'
+
+const App = () => {
+  return (
+    <div className='w-fit h-fit mx-auto p-4 mt-10 bg-slate-100'>
+      <MedicalCalendar />
+    </div>
+  )
+}
+
+export default App
